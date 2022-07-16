b9689605d76534f2ca0e2e044e6f062473231d35
Roshan Ekka <1ROSHANEKKA@GMAIL.COM>
2022-07-17 03:11:33 +0530

    made button redirect to a webpage

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+function register(params) {
+    location.href="https://www.twitter.com/1roshanekka"    
+}
+
+//this wouldn't allow to go back
+// function register(params) {
+//     location.replace("https://www.twitter.com/1roshanekka")
+// }
