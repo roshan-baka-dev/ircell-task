ce1c750e131fbc20d52aa221be38791abda1757c
Roshan Ekka <1ROSHANEKKA@GMAIL.COM>
2022-07-17 23:59:56 +0530

    added new colleges

--- app.js
+++ app.js
@@ -5,9 +5,25 @@ function register(params) {
 }
 
 
-function northamerica(params) {
-    location.href="/north-america.html"    
+function redirect(params) {
+    location.href="index.html"    
 }
+function redirect_northamerica(params) {
+    location.href="north-america.html"    
+}
+function redirect_southamerica(params) {
+    location.href="south-america.html"    
+}
+function redirect_asia(params) {
+    location.href="asia.html"    
+}
+function redirect_europe(params) {
+    location.href="europe.html"    
+}
+function redirect_australia(params) {
+    location.href="australia.html"    
+}
+
 
 //this wouldn't allow to go back
 // function register(params) {
