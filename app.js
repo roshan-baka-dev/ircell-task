8836a67a233d4c91755343d13ef57eacbd478a87
Roshan Ekka <1ROSHANEKKA@GMAIL.COM>
2022-07-17 21:43:26 +0530

    new file

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ function register(params) {
     location.href="https://www.twitter.com/1roshanekka"    
 }
 
+
+function northamerica(params) {
+    location.href="north-america.html"    
+}
+
 //this wouldn't allow to go back
 // function register(params) {
 //     location.replace("https://www.twitter.com/1roshanekka")
